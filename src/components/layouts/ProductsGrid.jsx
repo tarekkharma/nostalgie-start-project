b11a4c36ef92e630546cbc8fbca6b2e80bef58fa
Tarek Kharma <tarek95ramos@gmail.com>
2024-07-09 03:18:13 +0200

    fix padding and layout

--- src/components/layouts/ProductsGrid.jsx
+++ src/components/layouts/ProductsGrid.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "../../assets/partiels/products-grid.scss";
+import SectionTitle from "../SectionTitle";
 
 function ProductsGrid(props) {
   return (
     <section className={`products-grid ${props.className}`}>
       <div className="title">
-        <h2>{props.title}</h2>
+        <SectionTitle title={props.title} />
       </div>
       <div className="content">{props.children}</div>
       <div className="grid-footer">
